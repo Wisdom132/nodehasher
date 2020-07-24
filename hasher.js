ab721285c4d1b91658bf2637fea146ba53ee5d0f
Wisdom Ekpot <wisdomekpot@gmail.com>
2020-07-24 17:51:15 +0100

    compare features

--- hasher.js
+++ hasher.js
@@ -1,46 +1,59 @@
-let crypto = require("crypto");
-// logger 
-let logger = func => {
-  console.log(func)
-}
-//generate salt
-exports.generateSalt = rounds => {
-  if (rounds == null) {
-    rounds = 12
-  } else if (typeof rounds !== "number") {
-    throw new Error('Round Param must be a number');
+let crypto = require('crypto');
+// logger
+let logger = (func) => {
+  console.log(func);
+};
+exports.generateSalt = (rounds) => {
+  if (rounds >= 15) {
+    throw new Error(`${rounds} is greater than 15,Must be less that 15`);
   }
-  return crypto.randomBytes(Math.ceil(rounds / 2)).toString('hex').slice(0, rounds)
-}
+  if (typeof rounds !== 'number') {
+    throw new Error('rounds param must be a number');
+  }
+  if (rounds == null) {
+    rounds = 12;
+  }
+
+  return crypto.randomBytes(Math.ceil(rounds / 2)).toString('hex').slice(0, rounds);
+};
 
 // hasher function
 let hasher = (password, salt) => {
   let hash = crypto.createHmac('sha512', salt);
-  hash.update(password)
-  let value = hash.digest('hex')
+  hash.update(password);
+  let value = hash.digest('hex');
   return {
     salt: salt,
     hashedpassword: value
-  }
-}
+  };
+};
 
 //method to return hash and salt
 exports.hash = (password, salt) => {
   if (password == null || salt == null) {
     throw new Error('Must Provide Password and salt values');
   }
-  if (typeof password !== 'string' || (typeof salt !== 'string' && typeof salt !== 'number')) {
+  if (typeof password !== 'string' || typeof salt !== 'string') {
     throw new Error('password must be a string and salt must either be a salt string or a number of rounds');
   }
-  return hasher(password, salt)
-}
+  return hasher(password, salt);
+};
 
-exports.compare = (data, hash) => {
-  if (data == null || hash == null) {
-    throw new Error('data and hash arguments required');
+exports.compare = (password, hash) => {
+  hash = {
+    salt: '8c1a1c8575',
+    hashedpassword: '3f5b726ab32c8892a51d99db936ea4e8f1601cd0966cf1df5e1563a7aeb413c7597b06fdf71b3bbb3a44f427feaa87db1e66c0c5e6afd789c9a0016bb0aadad1'
   }
-  if (typeof data !== 'string' || typeof hash !== 'string') {
-    throw new Error('data and hash must be strings');
+  if (password == null || hash == null) {
+    throw new Error('password and hash is required to compare');
   }
+  if (typeof password !== 'string' || typeof hash !== 'object') {
+    throw new Error('password must be a String and hash must be an Object');
+  }
+  let passwordData = hasher(password, hash.salt);
+  if (passwordData.hashedpassword === hash.hashedpassword) {
+    return true;
+  }
+  return false
 
-}
+};
--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 const hasher = require('./hasher');
-let salt = hasher.generateSalt(12);
+let salt = hasher.generateSalt(10);
 let test = hasher.hash('wisdom', salt);
-
 console.log(test)
+
+let compare = hasher.compare('wisdom');
+console.log({
+    compare: compare
+})
